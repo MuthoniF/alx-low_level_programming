c5cde56b2f87e87f81b66207b56ead1b1f2f2dfd
Fridah <worknera @gmail.com>
2023-03-27 20:48:16 +0300

    Task 1

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,10 +1,13 @@
 #include <unistd.h>
 
 /**
- * Writes a single character to the standard output stream.
- * @param c the character to write
- * @return the character written, or -1 on error
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c) {
-    return write(STDOUT_FILENO, &c, 1);
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
