272529a05602f5723301e48cb44104cf78d6a7b0
Fridah <worknera@gmail.com>
2023-04-06 05:25:27 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _strlen_recursion - Function to return string length
+*@s:string
+*
+*Return: string length
+*/
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s)
+	{
+		length++;
+		length += _strlen_recursion(s + 1);
+	}
+
+	return (length);
+}
